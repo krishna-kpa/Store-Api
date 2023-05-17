13401598d27957af0398d6d47a2b9f17373956cb
Krishna Prasad A <kpa.krishnaprasada@gmail.com>
2023-05-17 23:26:42 +0530

    completed
    completed added queries

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require("express");
+const app = express();
+
+
+const connectDB = require('./db/connect');
+const products = require('./models/product')
+const productsRouter = require("./routes/products");
+
+const errorMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+
+
+// Middleware
+app.use(express.json());
+
+
+//Routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+  });
+
+
+app.use('/api/v1/products', productsRouter);
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+
+const port = process.env.PORT || 3000
+const productList = require('./products.json')
+
+const start = async ()=>{
+    try{
+        // DB connect
+        await connectDB(process.env.Mongo_URI)
+        await products.deleteMany()
+        await products.create(productList)
+        console.log("Connected to DB...")
+        app.listen(port,(err)=>{
+            if(err){
+                console.log("Cant connect on "+port)
+            }else{
+                console.log("Connected on "+port)
+            }
+        })
+    }catch(err){
+        console.log("Error Occured..."+err)
+    }
+}
+
+start();
---
+++ controllers/products.js
@@ -0,0 +1,77 @@
+const product = require('../models/product')
+
+
+
+
+const getAllProducts  = async (req,res)=>{
+    const {featured,company,name,sort,fields,numericFilter} = req.query
+    const queryObject = {}
+    // General Queries
+    if(featured){
+        queryObject.featured = featured=='true'?true:false;
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = {$regex:name, $options:"i"}
+    }
+
+    //Numeric Filters
+
+    if(numericFilter){
+
+        const operatorMap={
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte'
+        }
+
+        const regex = /\b(>|>=|=|<|<=)\b/g
+        let filters = numericFilter.replace(
+            regex,
+            (match)=>'-'+operatorMap[match]+'-'
+        )
+        let options = ['price','rating']
+        filters = filters.split(',').forEach((item)=>{
+            let [field,operator,value]=item.split('-')
+            if(options.includes(field)){
+                queryObject[field]={[operator]:Number(value)}
+            }
+        })
+    }
+
+
+
+
+    let result = product.find(queryObject)
+    //sorting
+    if(sort){
+        let sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    }else{
+        result = result.sort('createdAt')
+    }
+    //selecting fields
+    if(fields){
+        let fieldList = fields.split(',').join(' ')
+        result = result.select(fieldList)
+    }else{
+        result = result
+    }
+    // pagination
+    let page = Number(req.query.page)||1
+    let limit = Number(req.query.limit)||10
+    let skip = (page-1)*limit
+    result = result.skip(skip).limit(limit)
+    let products = await result
+    res.status(200).json({nbHits:products.length,Products : products})
+}
+const getAllProductsStatic = async (req,res)=>{
+    const products = await product.find()
+    res.json({product:products,nbHits:products.length})
+}
+
+module.exports = {getAllProducts,getAllProductsStatic}
